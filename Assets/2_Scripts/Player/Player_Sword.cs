a5a370f1c3e24fb770562bf5e7ab3b62835d012b
Justin Lam <justinflam9225k@gmail.com>
2025-02-24 16:30:27 -0800

    Implemented combo attacking

--- Assets/2_Scripts/Player/Player_Sword.cs
+++ Assets/2_Scripts/Player/Player_Sword.cs
@@ -2,22 +2,38 @@ using UnityEngine;
 
 public class Player_Sword : MonoBehaviour
 {
-	[SerializeField] float damage;
-	[SerializeField] float cooldown;
-	[SerializeField] float[] comboDurations;
-	float cooldownCounter;
+	[System.Serializable] class ComboAttack
+	{
+		[field: SerializeField] public float damage { get; private set; }
+		[field: SerializeField, Tooltip("How long the attack lasts")] public float duration { get; private set; }
+		[field: SerializeField, Tooltip("How long the player has from the previous attack to do this one")] public float buffer { get; private set; }
+	}
+
+	[SerializeField] ComboAttack[] attackCombo;
+	int comboIndex = 0;
+	float durationCounter = 0;
+	float bufferCounter = 0;
 
 	void OnAttack()
 	{
-		if (cooldownCounter > 0) return;
+		if (durationCounter > 0) return;  // if in middle of attack, don't attack
 
-		Debug.Log("attack 1");
+		if (comboIndex > 0 && bufferCounter <= 0) comboIndex = 0;	// cancel combo
 
-		cooldownCounter = cooldown;
+		Debug.Log($"Attack {comboIndex}"); // execute attack
+
+		int nextIndex = (comboIndex + 1) % attackCombo.Length;
+		ComboAttack attack = attackCombo[comboIndex];
+		ComboAttack nextAttack = attackCombo[nextIndex];
+
+		durationCounter = attack.duration;
+		bufferCounter = attack.duration + nextAttack.buffer;
+		comboIndex = nextIndex;
 	}
 
 	void Update()
 	{
-		if (cooldownCounter > 0) cooldownCounter -= Time.deltaTime;
+		if (durationCounter > 0) durationCounter -= Time.deltaTime;
+		if (bufferCounter > 0) bufferCounter -= Time.deltaTime;
 	}
 }
